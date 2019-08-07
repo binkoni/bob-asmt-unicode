7f46fb8a0a0b899aff7ed02c8169372b6c161e17
Byeonggon Lee <gonny952@gmail.com>
2019-08-07 20:32:03 +0900

    It works

--- unicode.py
+++ unicode.py
@@ -8,19 +8,16 @@ with open(sys.argv[1], mode='rb') as f:
     for i in range(len(lines)):
       if i + 1 < len(lines):
         nextLine = lines[i + 1]
-      print('line %d' % i)
-      print(line)
-      if i == 4:
-        print('line %d is %s' % (i, line.decode('utf-8')))
-      elif len(nextLine) >=3 and nextLine[0] == 0x00 and nextLine[1] == 0x00 and nextLine[2] == 0x00:
-        print('line %d is %s' % (i, line.decode('utf-32')))
+      #print(line)
+      if len(nextLine) >=3 and nextLine[0] == 0x00 and nextLine[1] == 0x00 and nextLine[2] == 0x00:
+        print('line %d (utf-32) %s' % (i, line.decode('utf-32')))
         line = nextLine[3:]
       elif len(nextLine) >= 1 and nextLine[0] == 0x00:
-        print('line %d is %s' % (i, line.decode('utf-16')))
+        print('line %d (utf-16): %s' % (i, line.decode('utf-16')))
         line = nextLine[1:]
       else:
-        print('line %d is %s' % (i, line.decode('cp949')))
+        try:
+          print('line %d (cp949): %s' % (i, line.decode('cp949')))
+        except:
+          print('line %d (utf-8): %s' % (i, line.decode('utf-8')))
         line = nextLine
-
-
-
