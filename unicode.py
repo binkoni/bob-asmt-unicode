c0e756fbdf0f226dd8120d9102d9aec1ed4a2c7e
Byeonggon Lee <gonny952@gmail.com>
2019-08-07 20:45:15 +0900

    Modify unicode.py

--- unicode.py
+++ unicode.py
@@ -1,7 +1,7 @@
 import sys
-if len(sys.argv) < 2:
+if len(sys.argv) < 3:
     sys.exit(1)
-with open(sys.argv[1], mode='rb') as f:
+with open(sys.argv[1], mode='rb') as f, open(sys.argv[2], mode='w') as fo:
     lines = f.read()
     lines = lines.split(b'\x0A')
     line = lines[0]
@@ -10,14 +10,14 @@ with open(sys.argv[1], mode='rb') as f:
         nextLine = lines[i + 1]
       #print(line)
       if len(nextLine) >=3 and nextLine[0] == 0x00 and nextLine[1] == 0x00 and nextLine[2] == 0x00:
-        print('line %d (utf-32) %s' % (i, line.decode('utf-32')))
+        fo.write('line %d (utf-32) %s\n' % (i, line.decode('utf-32')))
         line = nextLine[3:]
       elif len(nextLine) >= 1 and nextLine[0] == 0x00:
-        print('line %d (utf-16): %s' % (i, line.decode('utf-16')))
+        fo.write('line %d (utf-16): %s\n' % (i, line.decode('utf-16')))
         line = nextLine[1:]
       else:
         try:
-          print('line %d (cp949): %s' % (i, line.decode('cp949')))
+          fo.write('line %d (cp949): %s\n' % (i, line.decode('cp949')))
         except:
-          print('line %d (utf-8): %s' % (i, line.decode('utf-8')))
+          fo.write('line %d (utf-8): %s\n' % (i, line.decode('utf-8')))
         line = nextLine
